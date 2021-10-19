0451af7b7d5e176fbaef1e57952bdbbf02918e17
Abderrahmane Sfaihi <asfaihi@e1r5p8.1337.ma>
2021-10-19 11:20:33 +0100

    ex00 Megaphone

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/15 11:56:51 by asfaihi           #+#    #+#             */
+/*   Updated: 2021/10/15 12:13:41 by asfaihi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int     main(int argc, char **argv)
+{
+	int     i;
+	int     j;
+	char	c;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			c = toupper(argv[i][j]);
+			std::cout << c;
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
