4911edaa6e63bf3ab11f8e1c2d4e9635889731c6
Abderrahmane Sfaihi <asfaihi@e1r5p8.1337.ma>
2021-10-26 12:17:48 +0100

    Finished ex04

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -6,30 +6,37 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 13:05:35 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/10/25 14:16:55 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/10/26 12:16:14 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#define YELLOW "\e[1;33m"
 
 int     main( int argc, char **argv ) {
-	std::ifstream   ifs(argv[1]);
-	std::ofstream   ofs("new.out");
-	std::string     blop;
-	int				pos = 0;
+	if ( argc == 4 ) {
+		std::string		filename;
+		filename = argv[1];
+
+		std::ifstream   ifs(argv[1]);
+		std::ofstream   ofs(filename.append(".replace"));
+		std::string     blop;
+		int				pos = 0;
 	
-	if ( argc == 2 ) {
 		while ( std::getline(ifs, blop) ) {
-			pos = blop.find( "std", pos );
-			if (pos != -1) {
-				blop.erase( pos, 3 );
-				blop.insert( pos, "AAAA" );
+			pos = 0;
+			while (pos != -1) {
+				pos = blop.find( argv[2], pos );
+				if (pos != -1) {
+					blop.erase( pos, strlen(argv[2]) );
+					blop.insert( pos, argv[3] );
+				}
 			}
 			ofs << blop << std::endl;
 		}
 	}
 	else
-		std::cout << "Invalid arguments." << std::endl;
+		std::cout << YELLOW << "Invalid arguments." << std::endl;
 	return (0);
 }
