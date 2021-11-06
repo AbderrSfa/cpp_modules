1abc518c20d5473d3ea430c6dbc3f4ad301438c0
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2021-11-06 12:07:42 +0100

    Working on ex00

--- cpp_module_05/ex00/Bureaucrat.hpp
+++ cpp_module_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 15:56:31 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/11/04 15:57:51 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/06 12:06:19 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,31 @@
 # define BUREAUCRAT_HPP
 
 # include <iostream>
+# include <exception>
 
 class	Bureaucrat {
-	
+	private:
+		std::string const	_Name;
+		int					_Grade;
+
+	public:
+		Bureaucrat(std::string const aName, int aGrade);
+		Bureaucrat(Bureaucrat const & src) ;
+		Bureaucrat & operator=(Bureaucrat const & rhs);
+		~Bureaucrat();
+
+		std::string	const getName( void ) const ;
+		int			getGrade( void ) const ;
+
+		void		IncrementGrade( void );
+		void		DecrementGrade( void );
 };
+
+class	GradeTooHighException : public std::exception {
+	public:
+		
+};
+
+std::ostream &	operator<<(std::ostream & o, Bureaucrat const & rhs);
 
 #endif
--- cpp_module_05/ex00/main.cpp
+++ cpp_module_05/ex00/main.cpp
@@ -6,12 +6,22 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 15:55:26 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/11/04 15:59:08 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/06 11:59:39 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int		main() {
+	try {
+		Bureaucrat	yoink("Gagooch", 15);
+		std::cout << yoink << std::endl;
+		yoink.IncrementGrade();
+		std::cout << yoink << std::endl;
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
 	
+
 }
