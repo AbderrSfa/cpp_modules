6a92c666f53142f1a72b078be150bd9c508bfecf
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2021-11-06 12:11:22 +0100

    Created files for d05/ex01

---
+++ cpp_module_05/ex01/Bureaucrat.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 15:59:13 by asfaihi           #+#    #+#             */
+/*   Updated: 2021/11/06 11:14:29 by asfaihi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(std::string const aName, int aGrade) : _Name(aName), _Grade(aGrade) {
+	std::cout << "Bureaucrat is born." << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const & src) : _Name(src._Name) {
+	*this = src;
+}
+
+Bureaucrat & Bureaucrat::operator=(Bureaucrat const & rhs) {
+	if (this == &rhs)
+		return (*this);
+	this->_Grade = rhs._Grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {
+	std::cout << "Bureaucrat died." << std::endl;
+}
+
+std::string const Bureaucrat::getName( void ) const { return (this->_Name); }
+
+int		Bureaucrat::getGrade( void ) const { return (this->_Grade); }
+
+void	Bureaucrat::IncrementGrade( void ) { this->_Grade--; }
+
+void	Bureaucrat::DecrementGrade( void ) { this->_Grade++; }
+
+std::ostream &	operator<<(std::ostream & o, Bureaucrat const & rhs) {
+	o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << ".";
+	return (o); 
+}
