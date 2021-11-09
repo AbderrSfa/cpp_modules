a4be56063f84805c6201b3b75c75d74e096aaebf
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2021-11-09 09:46:12 +0100

    Changed some stuff in D01/ex02

--- cpp_module_01/ex02/main.cpp
+++ cpp_module_01/ex02/main.cpp
@@ -6,22 +6,22 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 13:06:14 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/10/25 13:06:15 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/09 09:45:30 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 int		main() {
-	std::string		Brain = "HI THIS IS BRAIN";
-	std::string*	stringPTR = &Brain;
-	std::string&	stringREF = Brain;
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
 
-	std::cout << &Brain << std::endl;
-	std::cout << stringPTR << std::endl;
-	std::cout << &stringREF << std::endl;
+	std::cout << "The address of the string: \t\t\t" << &str << std::endl;
+	std::cout << "The address of the string using stringPTR:\t" << stringPTR << std::endl;
+	std::cout << "The address of the string using stringREF:\t" <<  &stringREF << std::endl;
 	std::cout << std::endl;
 
-	std::cout << *stringPTR << std::endl;
-	std::cout << stringREF << std::endl;
+	std::cout << "Display the string using the pointer:\t\t" <<  *stringPTR << std::endl;
+	std::cout << "Display the string using the reference:\t\t" <<  stringREF << std::endl;
 }
