bc2e868e69aaea2e3d28da12466909f84fe74cae
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2021-11-12 10:36:27 +0100

    Fixing tiny mistakes + making base class attributes protected

--- cpp_module_03/ex03/DiamondTrap.cpp
+++ cpp_module_03/ex03/DiamondTrap.cpp
@@ -6,25 +6,24 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 10:55:34 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/11/12 10:10:59 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/12 10:29:51 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
 DiamondTrap::DiamondTrap( void ) {
-/* 	this->setHitpoints(100);
-	this->setEnergyPoints(50);
-	this->setAttackDamage(30); */
-	this->_Hitpoints = FragTrap::_Hitpoints;
+	this->_HitPoints = FragTrap::_HitPoints;
+	this->_EnergyPoints = ScavTrap::_EnergyPoints;
+	this->_AttackDamage = FragTrap::_AttackDamage;
 	std::cout << "DiamondTrap default constructor called" << std::endl;
 }
 
 DiamondTrap::DiamondTrap(std::string aName) : ClapTrap(aName + "_clap_name") {
-	this->setName(aName);
-	this->setHitpoints(100);
-	this->setEnergyPoints(50);
-	this->setAttackDamage(30);
+	this->_Name = aName;
+	this->_HitPoints = FragTrap::_HitPoints;
+	this->_EnergyPoints = ScavTrap::_EnergyPoints;
+	this->_AttackDamage = FragTrap::_AttackDamage;
 	std::cout << "DiamondTrap parameterized constructor called" << std::endl;
 }
 
@@ -36,10 +35,10 @@ DiamondTrap::DiamondTrap(DiamondTrap const & src) {
 DiamondTrap &  DiamondTrap::operator=(DiamondTrap const & rhs) {
 	if (this == &rhs)
 		return *this;
-	this->setName(rhs.getName());
-	this->setAttackDamage(rhs.getAttackDamage());
-	this->setHitpoints(rhs.getHitPoints());
-	this->setEnergyPoints(rhs.getEnergyPoints());
+	this->_Name = rhs._Name;
+	this->_AttackDamage = rhs._AttackDamage;
+	this->_HitPoints = rhs._HitPoints;
+	this->_EnergyPoints = rhs._EnergyPoints;
 	return *this;
 }
 
