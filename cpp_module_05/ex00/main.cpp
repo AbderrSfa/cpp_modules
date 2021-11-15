706147727f41c06f4e40e0a3b8f57c429fdc1063
AbderrSfa <abderrahmanesfaihi@hotmail.com>
2021-11-15 13:17:25 +0100

    Tiny changes

--- cpp_module_05/ex00/main.cpp
+++ cpp_module_05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 15:55:26 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/11/08 10:22:45 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/15 12:09:12 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int		main() {
 	try {
-		Bureaucrat	yoink("Gagooch", 180);
-		std::cout << yoink << std::endl;
-		yoink.DecrementGrade();
-		std::cout << yoink << std::endl;
+		Bureaucrat	bookie("Mike", 100);
+		std::cout << bookie << std::endl;
+		bookie.DecrementGrade();
+		std::cout << bookie << std::endl;
 	}
 	catch(std::exception& e) {
 		std::cerr << e.what() << '\n';
