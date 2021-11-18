f480aacbde9cbf2a2b3e8b4cdc80de6a5f04db70
Abderrahmane Sfaihi <asfaihi@e1r6p5.1337.ma>
2021-11-18 14:59:59 +0100

    Starting ex00

--- cpp_module_06/ex00/main.cpp
+++ cpp_module_06/ex00/main.cpp
@@ -1,8 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 13:19:26 by asfaihi           #+#    #+#             */
+/*   Updated: 2021/11/18 14:42:12 by asfaihi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-    double f = 3.123123123;
-    double b = f;
-    std::cout << b;
+	if (argc != 2) {
+		std::cout << "Invalid arguments." << std::endl;
+		return (1);
+	}
+	int		i;
+	char	c;
+	float	fl;
+	double	d;
+
+	i = static_cast<int>(atoi(argv[1]));
+	c = static_cast<char>(i);
+	fl = static_cast<float>(i);
+	d = static_cast<double>(i);
+
+	if (c > 32)
+		std::cout << "char: '" << c << "'" << std::endl;
+	else
+		std::cout << "char: Non displayable" << std::endl;
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << fl << "f" << std::endl;
+	std::cout << "double: " << d << std::endl;
 }
