b04ebdd704d42783979dd72786baf05b0839d5a0
Abderrahmane Sfaihi <asfaihi@e1r6p5.1337.ma>
2021-11-22 16:49:27 +0100

    Started D07

---
+++ cpp_module_07/ex00/main.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 15:30:50 by asfaihi           #+#    #+#             */
+/*   Updated: 2021/11/22 16:43:08 by asfaihi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+template<typename T>
+void	swap(T& a, T& b) {
+	T	temp(a);
+	a = b;
+	b = temp;
+}
+
+template<typename T>
+T		min(T& a, T& b) {
+	if (b <= a)
+		return (b);
+	return (a);
+}
+
+template<typename T>
+T		max(T& a, T& b) {
+	if (b >= a)
+		return (b);
+	return (a);		
+}
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return (0);
+}
