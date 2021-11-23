5dbb3f1e4271092eb359c0b220e5c9cc8f7766bf
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-11-23 22:16:40 +0100

    Finished D07

--- cpp_module_07/ex02/Array.hpp
+++ cpp_module_07/ex02/Array.hpp
@@ -14,50 +14,58 @@
 # define ARRAY_HPP
 
 # include <iostream>
+# include <cstdlib>
 
 template<typename T>
 class Array
 {
+	private:
+		T				*_Arr;
+		unsigned int	_Size;
+
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(Array const & src);
-		Array & operator=(Array const & rhs);
-		~Array();
+		Array() : _Arr(0), _Size(0) {};
+		Array(unsigned int n) : _Arr(new T[n]), _Size(n) {}
+		Array(Array const & src) { *this = src; }
+		~Array() { delete [] this->_Arr; };
 
-		T	*_Arr;
+		Array & operator=(Array const & rhs) {
+			if (this == &rhs)
+				return (*this);
+			this->_Size = rhs._Size;
+			this->_Arr = new T[this->_Size];
+			for (size_t i = 0; i < this->_Size; i++) {
+				this->_Arr[i] = rhs._Arr[i];
+			}	
+			return (*this);
+		}
 
-		void	print( void );
+		class	OutOfLimits : public std::exception {
+			const char * what() const throw() {
+				return ("Exception: Element is out of bounds.");
+			}
+		};
+
+		T &			operator[](unsigned int index) {
+			if (index >= this->_Size)
+				throw OutOfLimits();
+			return (this->_Arr[index]);
+		};
+
+		const T &	operator[](unsigned int index) const {
+			if (index >= this->_Size)
+				throw OutOfLimits();
+			return (this->_Arr[index]);
+		};
+
+		unsigned int size() const { return (this->_size); };
+
+		void	print( void ) {
+			for (size_t i = 0; i < 4; i++)
+			{
+				std::cout << this->_Arr[i] << " ";
+			}
+		};
 };
-
-template<typename T>
-Array<T>::Array() {}
-
-template<typename T>
-Array<T>::Array(unsigned int n) {
-	this->_Arr = new T[n];
-}
-
-template<typename T>
-Array<T>::Array(Array const & src) { *this = src; }
-
-template<typename T>
-Array<T> & Array<T>::operator=(Array<T> const & rhs) {
-	if (this == &rhs)
-		return (*this);
-	this->_Arr = rhs._Arr;
-	return (*this);
-}
-
-template<typename T>
-Array<T>::~Array() {}
-
-template<typename T>
-void	Array<T>::print( void ) {
-	for (size_t i = 0; i < 4; i++)
-	{
-		std::cout << this->_Arr[i] << " ";
-	}
-}
 
 #endif
