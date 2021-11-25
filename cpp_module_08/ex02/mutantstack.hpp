caeb9a0cfe3cd477961683d0975469a587d86c10
Abderrahmane Sfaihi <asfaihi@e1r6p5.1337.ma>
2021-11-25 16:35:56 +0100

    Created files for d08/ex02

---
+++ cpp_module_08/ex02/mutantstack.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 16:16:55 by asfaihi           #+#    #+#             */
+/*   Updated: 2021/11/25 16:20:41 by asfaihi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+class mutantstack
+{
+	private:
+	
+	public:
+		mutantstack( void );
+		~mutantstack();
+};
+
+mutantstack::mutantstack( void )
+{
+}
+
+mutantstack::~mutantstack()
+{
+}
+
+
+#endif
