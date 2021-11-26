442b9258cca3ab791632fdcfc73f1bf4134c392f
Abderrahmane Sfaihi <asfaihi@e1r6p5.1337.ma>
2021-11-26 11:38:38 +0100

    Working on ex02

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -6,12 +6,28 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:16:52 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/11/25 16:17:18 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/26 11:37:51 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
 
 int		main() {
-	
+	mutantstack	ms;
+
+	ms.push(5);	
+	ms.push(17);	
+
+	std::cout << ms.top() << std::endl;
+
+	ms.pop();
+
+	std::cout << ms.size() << std::endl;
+
+	ms.push(3);
+	ms.push(5);
+	ms.push(737);
+	std::cout << ms.top() << std::endl;
+	ms.pop();
+	std::cout << ms.top() << std::endl;
 }
--- cpp_module_08/ex02/mutantstack.hpp
+++ cpp_module_08/ex02/mutantstack.hpp
@@ -6,29 +6,27 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:16:55 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/11/25 16:20:41 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/26 11:38:26 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-class mutantstack
+# include <iostream>
+# include <stack>
+# include <list>
+
+class mutantstack : public std::stack<int>
 {
 	private:
 	
 	public:
-		mutantstack( void );
-		~mutantstack();
+		mutantstack( void ) {}
+		mutantstack(mutantstack const & src) { *this = src; } ;
+		~mutantstack() {}
+		mutantstack & operator=(mutantstack const & rhs) { return (*this); };
+		//typedef typename std::stack<int>::container_type::iterator iterator;
 };
-
-mutantstack::mutantstack( void )
-{
-}
-
-mutantstack::~mutantstack()
-{
-}
-
 
 #endif
