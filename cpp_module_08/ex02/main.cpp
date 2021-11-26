c85fbf9d93208cc4fb687dc2476358ec182df7ec
Abderrahmane Sfaihi <asfaihi@e1r6p5.1337.ma>
2021-11-26 14:33:47 +0100

    Added more tests to main

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:16:52 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/11/26 13:11:54 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/26 14:05:53 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,61 @@
 
 int main()
 {
-	MutantStack<int> mstack;
-
-	mstack.push(5);
-	mstack.push(17);
-
-	std::cout << mstack.top() << std::endl;
-
-	mstack.pop();
-
-	std::cout << mstack.size() << std::endl;
-
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-
-	++it;
-	--it;
-	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+
+		std::cout << mstack.top() << std::endl;
+
+		mstack.pop();
+
+		std::cout << mstack.size() << std::endl;
+
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+
+		std::cout << std::endl;
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
 	}
-	std::stack<int> s(mstack);
+
+	std::cout << "-------------" << std::endl;
+
+	{
+		MutantStack<std::string> mstack;
+
+		mstack.push("abcd");
+		mstack.push("efgh");
+		mstack.push("ijkl");
+		mstack.push("mnop");
+		mstack.push("qrst");
+		mstack.push("uvwxyz");
+
+		std::cout << std::endl;
+
+		MutantStack<std::string>::iterator it = mstack.begin();
+		MutantStack<std::string>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	return 0;
 }
--- cpp_module_08/ex02/mutantstack.hpp
+++ cpp_module_08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:16:55 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/11/26 13:21:53 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/11/26 14:32:13 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ class MutantStack : public std::stack<T, Container>
 		~MutantStack() {}
 		MutantStack & operator=(MutantStack const & rhs) { return (*this); };
 
-		typedef typename std::stack<T, Container>::container_type::iterator	iterator;
-
+		typedef typename std::stack<T, Container>::container_type::iterator iterator;
+		
 		iterator begin() { return this->c.begin(); }
 		iterator end() { return this->c.end(); }
 };
