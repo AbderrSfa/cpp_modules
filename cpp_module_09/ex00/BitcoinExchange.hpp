86a60f68ac59dfd4a2738c4806dc66dd5be9579e
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2023-09-24 23:55:48 +0100

    Added necessary includes

--- cpp_module_09/ex00/BitcoinExchange.hpp
+++ cpp_module_09/ex00/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abderr <abderr@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abderrsfa <abderrsfa@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:42:07 by asfaihi           #+#    #+#             */
-/*   Updated: 2023/08/29 22:48:53 by abderr           ###   ########.fr       */
+/*   Updated: 2023/09/24 23:54:14 by abderrsfa        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,41 @@
 #include <iostream>
 #include <fstream>
 #include <map>
-#include <limits>
+#include <climits>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
 
 class BitcoinExchange
 {
 private:
-  struct Date
-  {
-    int year;
-    int month;
-    int day;
-  };
+	struct Date
+	{
+		int year;
+		int month;
+		int day;
+	};
 
-  std::map<std::string, double> _db;
+	std::map<std::string, double> _db;
 
-  void ft_error(std::string error_message, bool exit_program);
-  void fill_db();
-  std::vector<std::string> ft_split(const std::string &input, char delimiter);
-  void read_file();
-  std::string remove_spaces(const std::string &string);
-  bool is_valid_entry(const std::string &line, std::string &date, double &value);
-  int get_date(const std::string &date, Date &s_date);
-  bool is_valid_date(const Date &s_date);
-  bool is_leap_year(int year);
-  bool is_valid_number(const std::string &str);
-  BitcoinExchange(void);
+	void ft_error(std::string error_message, bool exit_program);
+	void fill_db();
+	std::vector<std::string> ft_split(const std::string &input, char delimiter);
+	void read_file();
+	std::string remove_spaces(const std::string &string);
+	bool is_valid_entry(const std::string &line, std::string &date, double &value);
+	int get_date(const std::string &date, Date &s_date);
+	bool is_valid_date(const Date &s_date);
+	bool is_leap_year(int year);
+	bool is_valid_number(const std::string &str);
+	BitcoinExchange(void);
 
 public:
-  std::string _filename;
-  BitcoinExchange(const std::string &filename);
-  BitcoinExchange(BitcoinExchange const &src);
-  BitcoinExchange &operator=(BitcoinExchange const &rhs);
-  ~BitcoinExchange();
+	std::string _filename;
+	BitcoinExchange(const std::string &filename);
+	BitcoinExchange(BitcoinExchange const &src);
+	BitcoinExchange &operator=(BitcoinExchange const &rhs);
+	~BitcoinExchange();
 };
 
 #endif
--- cpp_module_09/ex00/main.cpp
+++ cpp_module_09/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abderr <abderr@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abderrsfa <abderrsfa@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:42:09 by asfaihi           #+#    #+#             */
-/*   Updated: 2023/08/29 22:55:40 by abderr           ###   ########.fr       */
+/*   Updated: 2023/09/24 23:47:51 by abderrsfa        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 int main(int argc, char **argv)
 {
-  if (argc != 2)
-  {
-    std::cerr << "Error: wrong number of parameters." << std::endl;
-    exit(1);
-  }
-  BitcoinExchange btc(argv[1]);
-  return (0);
+	if (argc != 2)
+	{
+		std::cerr << "Error: wrong number of parameters." << std::endl;
+		exit(1);
+	}
+	BitcoinExchange btc(argv[1]);
+	return (0);
 }
